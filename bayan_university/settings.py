a60d2f49cb03b8b345a1bc734a57a5feadc8d46d
mynawam <mynawam@bluemint.ai>
2024-07-29 09:31:44 +0300

    update static files

--- bayan_university/settings.py
+++ bayan_university/settings.py
@@ -172,7 +172,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
